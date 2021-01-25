82754b65674b1a5d57f5a8b9511f074a700ce21e
lenconda <i@lenconda.top>
2021-01-25 13:08:55 +0800

    refactor: scaffold parsers

--- src/utils/scaffold.ts
+++ src/utils/scaffold.ts
@@ -1,25 +1,35 @@
+export type ScaffoldNameParser = (name: string) => string;
+
 /**
- * parse user's input and return a correct template name
- * @param name string
- * @returns string
+ * parser generator
+ * @param scaffoldPrefix string
+ * @param defaultNamespace string
  */
-const parseScaffoldName = (name: string): string => {
-  if (/\//.test(name)) {
-    const templateNameChunks = name.split('/');
-    return templateNameChunks.reduce((result, currentValue, currentIndex) => {
-      return `${currentIndex !== 0 ? `${result}/` : result}${
-        currentIndex === templateNameChunks.length - 1
-          ? currentValue.startsWith('scaffold-')
-            ? currentValue
-            : `scaffold-${currentValue}`
-          : currentValue
-      }`.trim();
-    }, '');
-  } else {
-    return name.startsWith('scaffold-')
-      ? `dolliejs/${name}`
-      : `dolliejs/scaffold-${name}`;
-  }
+const createParser = (
+  scaffoldPrefix: string,
+  defaultNamespace = 'dolliejs'
+): ScaffoldNameParser => {
+  return (name: string) => {
+    if (/\//.test(name)) {
+      const templateNameChunks = name.split('/');
+      return templateNameChunks.reduce((result, currentValue, currentIndex) => {
+        return `${currentIndex !== 0 ? `${result}/` : result}${
+          currentIndex === templateNameChunks.length - 1
+            ? currentValue.startsWith(scaffoldPrefix)
+              ? currentValue
+              : `${scaffoldPrefix}${currentValue}`
+            : currentValue
+        }`.trim();
+      }, '');
+    } else {
+      return name.startsWith(scaffoldPrefix)
+        ? `${defaultNamespace}/${name}`
+        : `${defaultNamespace}/${scaffoldPrefix}${name}`;
+    }
+  };
 };
 
-export { parseScaffoldName };
+const parseScaffoldName = createParser('scaffold-');
+const parseExtendScaffoldName = createParser('extend-scaffold-');
+
+export { parseScaffoldName, parseExtendScaffoldName };
