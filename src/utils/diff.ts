429e542f996ba4acf822116584e5abc94daf3004
lenconda <i@lenconda.top>
2021-01-31 00:07:19 +0800

    feat(docs): add jsdocs and comments

--- src/utils/diff.ts
+++ src/utils/diff.ts
@@ -5,6 +5,15 @@ import Generator from 'yeoman-generator';
 import { DollieScaffold, FileAction } from '../interfaces';
 import { isPathnameInConfig } from './scaffold';
 
+/**
+ * optimize diff algorithm and returns a more appropriate result
+ * @param originalContent string
+ * @param newContent string
+ * @returns Change[]
+ *
+ * this function uses diff algorithm from Myers: http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.4.6927
+ * we diff two blocks of text with lines, and split the values with single lines
+ */
 const diff = (originalContent: string, newContent: string): Change[] => {
   const changes = diffLines(originalContent, newContent);
   const result = changes.reduce((result, currentItem) => {
@@ -13,52 +22,93 @@ const diff = (originalContent: string, newContent: string): Change[] => {
       : currentItem.value
     )
       .split('\n')
-      .map((item) =>
-        _.omit(
-          {
-            ...currentItem,
-            value: `${item}\n`,
-          },
-          'count'
-        )
-      );
+      /**
+       * remove `count` which means the total count of lines in each change
+       * because each change will only be one line
+       */
+      .map((item) => _.omit({ ...currentItem, value: `${item}\n` }, 'count'));
     return result.concat(lines);
   }, []);
 
   return result;
 };
 
+/**
+ * merge two changes records into one
+ * @param currentChanges Change[]
+ * @param newChanges Change[]
+ * @returns Change[]
+ *
+ * `currentChanges` is the changes between original text and current text
+ * `newChanges` is the changes between current text and new text
+ */
 const merge = (currentChanges: Change[], newChanges: Change[]): Change[] => {
-  const result = [];
-  const removedQueue = currentChanges.filter((change) => change.removed);
-  const addedQueue = currentChanges.filter((change) => change.added);
-  if (removedQueue.length === 0 && addedQueue.length === 0) {
+  /**
+   * the final changes from the merge result
+   * it is also in the type `Change[]`
+   */
+  const result: Change[] = [];
+  const removedChangesList = currentChanges.filter((change) => change.removed);
+  const addedChangesList = currentChanges.filter((change) => change.added);
+  /**
+   * if `removedChangesList` and `addedChangesList` are all empty, it means
+   * there is no change been made between original text and current text, so
+   * we just return the new changes without removed ones
+   */
+  if (removedChangesList.length === 0 && addedChangesList.length === 0) {
     return newChanges.filter((change) => !change.removed);
   }
+  /**
+   * traverse all changes in `newChanges`
+   */
   for (const newChange of newChanges) {
+    /**
+     * for each removed change in `newChanges`
+     * if there is a same value in `addedChangesList`, which means new text should not remove
+     * current change
+     * so we should push current change to `result`, no matter will be removed by new text
+     * after pushing, we should delete it from `addedChangesList`
+     */
     if (newChange.removed) {
-      if (addedQueue.length > 0) {
-        const addedChangeIndex = addedQueue.findIndex(
+      if (addedChangesList.length > 0) {
+        const addedChangeIndex = addedChangesList.findIndex(
           (item) => item.value === newChange.value
         );
         if (addedChangeIndex !== -1) {
           result.push(newChange);
-          addedQueue.splice(addedChangeIndex, 1);
+          addedChangesList.splice(addedChangeIndex, 1);
         }
       }
+      /**
+       * for each added change in `newChanges`
+       * if there is not a same value in `removedChangesList`, which means current change is not
+       * redundant, that is, this change should be applied to the result
+       * so we should push it to `result`
+       * if we find an item with the same value in `removedChangesList`, we should not push current
+       * change to `result`, but delete the item from `removedChangesList`
+       */
     } else if (newChange.added) {
-      if (removedQueue.length === 0) {
+      /**
+       * if `removedChangesList` is empty, it means that all added changes from new text should
+       * be applied to the result
+       */
+      if (removedChangesList.length === 0) {
         result.push(newChange);
       } else {
-        const removedChangeIndex = removedQueue.findIndex(
+        const removedChangeIndex = removedChangesList.findIndex(
           (item) => item.value === newChange.value
         );
         if (removedChangeIndex === -1) {
           result.push(newChange);
         } else {
-          removedQueue.splice(removedChangeIndex, 1);
+          removedChangesList.splice(removedChangeIndex, 1);
         }
       }
+      /**
+       * if current change has neither `removed` nor `added` flag, it means the change is the same
+       * both in current text and new text
+       * so we should push it to `result`
+       */
     } else {
       result.push(newChange);
     }
@@ -66,6 +116,19 @@ const merge = (currentChanges: Change[], newChanges: Change[]): Change[] => {
   return result;
 };
 
+/**
+ * parse scaffold tree, temp files of each scaffold and user's scaffold configuration
+ * and return an appropriate file action strategy
+ * @param scaffold DollieScaffold
+ * @param tempPath string
+ * @param destinationPath string
+ * @param relativePathname string
+ * @param fs typeof Generator.prototype.fs
+ *
+ * @description `DIRECT` write file to destination pathname directory
+ * @description `MERGE` merge current text from destination file and new text
+ * @description `NIL` do nothing
+ */
 const checkFileAction = (
   scaffold: DollieScaffold,
   tempPath: string,
@@ -76,6 +139,10 @@ const checkFileAction = (
   const scaffoldFilesConfig =
     scaffold.configuration && scaffold.configuration.files;
 
+  /**
+   * if current scaffold does not have parent scaffold, which means it is the top-level scaffold
+   * so we just return `DIRECT`
+   */
   if (!scaffold.parent) {
     return 'DIRECT';
   }
@@ -86,6 +153,12 @@ const checkFileAction = (
     path.resolve(tempPath, scaffold.parent.uuid, relativePathname)
   );
 
+  /**
+   * if current scaffold has no configuration about how to deal with files, Dollie will consider
+   * overwriting all the files from current scaffold's temp dir
+   * so if the file exists in the destination dir, we should return `DIRECT`, otherwise, we should
+   * return `NIL` instead
+   */
   if (!scaffoldFilesConfig) {
     if (destFileExistence) {
       return 'DIRECT';
@@ -97,14 +170,31 @@ const checkFileAction = (
   const mergeConfig = _.get(scaffold.configuration, 'files.merge') || [];
   const addConfig = _.get(scaffold.configuration, 'files.add') || [];
 
+  /**
+   * if current file pathname matches `config.files.merge`, which means scaffold's author hope
+   * comparing this file's content with destination file's
+   * so if the file exists in destination dir, we should return `MERGE`, otherwise Dollie will
+   * consider adding it, that means, returns `DIRECT`
+   */
   if (isPathnameInConfig(relativePathname, mergeConfig)) {
     return destFileExistence && parentFileExistence ? 'MERGE' : 'DIRECT';
   }
 
+  /**
+   * if current file pathname matches `config.files.add`, which means scaffold's author hope
+   * adding this file to destination dir
+   * so if the file exists in destination dir, Dollie will consider comparing with current text
+   * which means returns `MERGE`, otherwise Dollie will consider adding it, that means, returns `DIRECT`
+   */
   if (isPathnameInConfig(relativePathname, addConfig)) {
-    return 'DIRECT';
+    return destFileExistence ? 'MERGE' : 'DIRECT';
   }
 
+  /**
+   * since Dollie uses a non-greedy strategy on adding new files, when current file pathname does
+   * not matches any rules above, Dollie will consider overwriting destination file with new text,
+   * however, if the file not exists in the destination dir, Dollie will return `NIL`
+   */
   return destFileExistence ? 'DIRECT' : 'NIL';
 };
 
--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -51,18 +51,18 @@ export const recursivelyWrite = (scaffold: DollieScaffold, context: DollieBaseGe
    * `scaffold.uuid` is the UUID for current scaffold, e.g. 3f74b271-04ac-4e7b-a5c1-b24894c529d2
    *
    * @example
-   * the `scaffoldDir` would probably be $HOME/.dollie/cache/3f74b271-04ac-4e7b-a5c1-b24894c529d2
+   * the `scaffoldSourceDir` would probably be $HOME/.dollie/cache/3f74b271-04ac-4e7b-a5c1-b24894c529d2
    */
-  const scaffoldDir = path.resolve(context.appBasePath, scaffold.uuid);
+  const scaffoldSourceDir = path.resolve(context.appBasePath, scaffold.uuid);
   const destinationDir = path.resolve(context.appTempPath, scaffold.uuid);
 
   /**
    * invoke `traverse` function in `src/utils/traverse.ts`, set the ignore pattern
-   * to avoid copying `.dollie.json` to destination path.
+   * to avoid copying `.dollie.json` to temporary dir
    */
-  traverse(path.resolve(scaffoldDir), TRAVERSE_IGNORE_REGEXP, (pathname: string, entity: string) => {
+  traverse(path.resolve(scaffoldSourceDir), TRAVERSE_IGNORE_REGEXP, (pathname: string, entity: string) => {
     /**
-     * `pathname` is an absolute pathname of file against `scaffoldDir` as above
+     * `pathname` is an absolute pathname of file against `scaffoldSourceDir` as above
      * we should get the relate pathname to concat with destination pathname
      *
      * @example
@@ -70,8 +70,8 @@ export const recursivelyWrite = (scaffold: DollieScaffold, context: DollieBaseGe
      * the `relativePath` would become as `src/index.js`
      */
     const relativePath = entity.startsWith('__template.')
-      ? `${path.relative(scaffoldDir, pathname).slice(0, 0 - entity.length)}${entity.slice(11)}`
-      : path.relative(scaffoldDir, pathname);
+      ? `${path.relative(scaffoldSourceDir, pathname).slice(0, 0 - entity.length)}${entity.slice(11)}`
+      : path.relative(scaffoldSourceDir, pathname);
     const destinationPathname = path.resolve(destinationDir, relativePath);
 
     if (entity.startsWith('__template.')) {
@@ -95,15 +95,57 @@ export const recursivelyWrite = (scaffold: DollieScaffold, context: DollieBaseGe
   }
 };
 
+/**
+ * copy and write files from temporary source
+ * @param scaffold DollieScaffold
+ * @param context DollieBaseGenerator
+ *
+ * once `recursivelyWrite` is finished, which means Dollie has written all files
+ * into the temporary dirs, then `recursivelyCopyToDestination` will be invoked to read
+ * the files from each file in each temporary dir and use an appropriate action to write
+ * the file content into destination dir
+ */
 export const recursivelyCopyToDestination = (scaffold: DollieScaffold, context: DollieBaseGenerator) => {
+  /**
+   * it is mentioned as above
+   * the dir storing all of the scaffold content on the physical file system
+   */
   const scaffoldSourceDir = path.resolve(context.appBasePath, scaffold.uuid);
+  /**
+   * the temporary dir on mem-fs for containing generated files with props
+   *
+   * @default
+   * $HOME/.dollie/temp/$UUID
+   */
   const scaffoldTempDir = path.resolve(context.appTempPath, scaffold.uuid);
 
+  /**
+   * invoke `traverse` function in `src/utils/traverse.ts`
+   * set the ignore pattern to avoid reading `.dollie.json` from temporary dir
+   * we still traverse from `scaffoldSourceDir` because it is the only way to
+   * get the folder structure from each nested scaffold
+   */
   traverse(scaffoldSourceDir, TRAVERSE_IGNORE_REGEXP, (pathname: string, entity: string) => {
+    /**
+     * get the relative path with the start dir of current scaffold's temporary dir
+     * still need get rid of `__template.` at the beginning of each file's filename
+     */
     const relativePathname = entity.startsWith('__template.')
       ? `${path.relative(scaffoldSourceDir, pathname).slice(0, 0 - entity.length)}${entity.slice(11)}`
       : path.relative(scaffoldSourceDir, pathname);
+    /**
+     * destination dir, into where Dollie will write the ultimate files
+     *
+     * @default
+     * process.cwd() + $PROJECT_NAME
+     */
     const destinationPathname = context.destinationPath(relativePathname);
+    /**
+     * get the action from relations as below:
+     * 1. parent scaffold's file content with the same name
+     * 2. file content in destination dir on mem-fs which has the same name as current one
+     * 3. current file that will be written into destination dir
+     */
     const action = checkFileAction(
       scaffold,
       context.appTempPath,
@@ -111,13 +153,31 @@ export const recursivelyCopyToDestination = (scaffold: DollieScaffold, context: 
       relativePathname,
       context.fs
     );
+    /**
+     * read current file from temporary dir on mem-fs
+     */
     const currentTempFileContent = context.fs.read(path.resolve(scaffoldTempDir, relativePathname));
     switch (action) {
+      /**
+       * if action for current file is `DIRECT`, which means we can directly write
+       * `currentTempFileContent` into destination file without worrying about previous content
+       */
       case 'DIRECT': {
         context.fs.delete(destinationPathname);
         context.fs.write(destinationPathname, currentTempFileContent);
         break;
       }
+      /**
+       * if action for current file is `MERGE`, which means we should take previous content
+       * into concern, so Dollie will do these things:
+       * 1. read the file with same name from parent scaffold's temp dir as `content1`
+       * 2. read current file content from destination dir as `content2`
+       * 3. read current file content from current scaffold's temp dir as `content3`
+       * 4. diff `content1` and `content2` as `diff1`
+       * 5. diff `content1` and `content3` as `diff2`
+       * 6. merge with `diff1` and `diff2` as `result`
+       * 7. write `result` into destination file
+       */
       case 'MERGE': {
         if (!scaffold.parent) {
           break;
@@ -133,6 +193,10 @@ export const recursivelyCopyToDestination = (scaffold: DollieScaffold, context: 
         context.fs.write(currentDestFilePath, result.map((item) => item.value).join(''));
         break;
       }
+      /**
+       * if action for current file is `NIL`, which means we should not take any action with
+       * current file content and destination file, even if creating and overwriting, just do nothing
+       */
       case 'NIL':
         break;
       default:
@@ -140,6 +204,10 @@ export const recursivelyCopyToDestination = (scaffold: DollieScaffold, context: 
     }
   });
 
+  /**
+   * if there are dependencies in current scaffold, then we should traverse the array
+   * and call `recursivelyCopyToDestination` to process array items
+   */
   for (const dependence of scaffold.dependencies) {
     recursivelyCopyToDestination(dependence, context);
   }
