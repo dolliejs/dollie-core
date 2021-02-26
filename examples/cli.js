6ffca2197ba3fd1a9711da88fcb4aacd105306a6
lenconda <i@lenconda.top>
2021-02-26 15:15:26 +0800

    refactor: project structure

---
+++ examples/cli.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const { parseComposeConfig } = require('../src/utils/compose');
+const dollie = require('../src/dollie');
+const log = require('../src/utils/log').default;
+
+async function test() {
+  const type = process.argv[2];
+  let config = {};
+
+  if (type === 'compose') {
+    const content = fs.readFileSync(path.resolve(__dirname, './config.yml'), {
+      encoding: 'utf-8',
+    });
+    config = parseComposeConfig(content);
+  }
+
+  if (type === 'container') {
+    config = {
+      projectName: 'project',
+      scaffoldConfig: {
+        scaffoldName: 'react',
+        dependencies: [
+          { scaffoldName: 'react-ts' },
+          { scaffoldName: 'react-less' },
+          { scaffoldName: 'react-dva' },
+        ],
+      },
+    };
+  }
+
+  try {
+    switch (type) {
+      case 'interactive': {
+        await dollie.interactive();
+        break;
+      }
+      case 'compose': {
+        await dollie.compose(config);
+        break;
+      }
+      case 'container': {
+        const manifest = await dollie.container(config);
+        console.log(manifest);
+        break;
+      }
+      default: break;
+    }
+  } catch (e) {
+    log.error(e.toString());
+    process.exit(1);
+  }
+}
+
+test();
